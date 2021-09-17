dc2c6e33f4190f4f036d22ee721a6554fd411423
Asif <asifaly310@gmail.com>
2021-09-17 18:09:17 +0500

    eval funciton removed

--- src/calculator-functions/calculation.js
+++ src/calculator-functions/calculation.js
@@ -1,4 +1,38 @@
 function calculate(result){
-    return eval(result);
+    console.log(result.split(''));
+    //const resultArray = result.split('');
+  //  const a = parseInt(resultArray[0]);
+
+  if(result.includes('+')){
+    const resultArray = result.split('+');
+    console.log(resultArray);
+    const a = parseInt(resultArray[0]);
+    const b = parseInt(resultArray[1]);
+    return a+b;
+  }
+  else if(result.includes('-')){
+    const resultArray = result.split('-');
+    console.log(resultArray);
+    const a = parseInt(resultArray[0]);
+    const b = parseInt(resultArray[1]);
+    return a-b;
+  }
+  else if(result.includes('*')){
+    const resultArray = result.split('*');
+    console.log(resultArray);
+    const a = parseInt(resultArray[0]);
+    const b = parseInt(resultArray[1]);
+    return a*b;
+  }
+
+  else if(result.includes('/')){
+    const resultArray = result.split('/');
+    console.log(resultArray);
+    const a = parseInt(resultArray[0]);
+    const b = parseInt(resultArray[1]);
+    return a/b;
+  }
+
+   
 }
 export default calculate;
